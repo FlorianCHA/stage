dce750fe592283f09356777928f7fc420daa90ac
florianCHA <florian.charriat@gmailm.com>
2018-07-05 14:04:19 +0200

    rapport

--- Presentation/aidePlotPrésentationEquipe.R
+++ Presentation/aidePlotPrésentationEquipe.R
@@ -39,7 +39,6 @@ for (i in 1:nrow(N50))
 }
 colnames(selection) <- colnames(dataQuality)
 rownames(selection) <- id.select
-dataQuality <- dataQuality[(40*8+1):nrow(dataQuality),]
 x.kmère <- c()
 for (i in 1:(nrow(dataQuality)/8))
 {
@@ -65,38 +64,62 @@ par(mfrow=c(2,2))
 plot(x = x.kmère, y = dataQuality$N50, ylab ='N50',xlab = 'Souche', xaxt="n" , main ='Graphique n°1 : Visualisation du N50\n de tous les assemblages', cex = 0.3)
 points(x = x.kmère[select.kmère], y = dataQuality$N50[select.kmère], cex = 0.4, col = 'red')
 abline(h=N50max,col="red")
-plot(x = x.kmère, y = dataQuality$L50, ylab ='L50', xlab = 'Souche', xaxt="n" , main ='Graphique n°1 : Visualisation du L50\n de tous les assemblage', cex = 0.3)
+plot(x = x.kmère, y = dataQuality$L50, ylab ='L50', xlab = 'Souche', xaxt="n" , main ='Graphique n°2 : Visualisation du L50\n de tous les assemblages', cex = 0.3)
 points(x = x.kmère[select.kmère], y = dataQuality$L50[select.kmère], cex = 0.4, col = 'red')
-plot(x = x.kmère, y = dataQuality$max,ylab ='max',xlab = 'Souche', xaxt="n" , main ='Graphique n°3 : Visualisation\n de la longueur max des contigs de tous les assemblagela longueur max des contigs', cex = 0.3)
+plot(x = x.kmère, y = dataQuality$max,ylab ='max',xlab = 'Souche', xaxt="n" , main ='Graphique n°3 : Visualisation de la longueur max\n des contigs de tous les assemblages', cex = 0.3)
 points(x = x.kmère[select.kmère], y = dataQuality$max[select.kmère], cex = 0.4, col = 'red')
-plot(x = x.kmère, y = dataQuality$n.500,ylab ='n.500',xlab = 'Souche', xaxt="n" , main ='Graphique n°4 : Visualisation\n du nombre de contigs supérieur à 500 de tous les assemblage' , cex = 0.3)
+plot(x = x.kmère, y = dataQuality$n.500,ylab ='n.500',xlab = 'Souche', xaxt="n" , main ='Graphique n°4 : Visualisationdu nombre de contigs\n supérieur à 500 de tous les assemblages' , cex = 0.3)
 points(x = x.kmère[select.kmère], y = dataQuality$n.500[select.kmère], cex = 0.4, col = 'red')
 
 
 
 ##########distribution longeur et GC ###############################
 
-data <- read.table('Documents/stage/StatQuality/stat_repeatMasker', header = T, sep  = '\t')
-plotLen <- ggplot(data, aes(x=Longueur.total)) + ggtitle("Distribution des longeurs de séquence\n de toutes les assemblages séléctionnés")+labs(x = "Longeur total de l'assemblage (mb)", y = 'Densité')+
+dataD <- read.table('Documents/stage/StatQuality/stat_repeatMasker', header = T, sep  = '\t')
+plotLen <- ggplot(data, aes(x=Longueur.total)) + ggtitle("Distribution des longeurs de séquence de toutes les assemblages séléctionnés")+labs(x = "Longeur total de l'assemblage (mb)", y = 'Densité')+
   geom_density(alpha=.2, fill="steelblue") + theme(axis.text.y = element_blank())
 plotLen
 
-plotMas <- ggplot(data, aes(x=Pourcentage.bases.masquées)) + ggtitle("Distribution de la proportion d'éléments masqués\n de toutes les assemblages séléctionnés")+labs(x = "% de nucléotide masquée", y = 'Densité')+
-  geom_density(alpha=.2, fill="steelblue") + theme(axis.text.y = element_blank()))
+plotMas <- ggplot(data, aes(x=Pourcentage.bases.masquées)) + ggtitle("Distribution de la proportion d'éléments masqués de toutes les assemblages séléctionnés")+labs(x = "% de nucléotide masquée", y = 'Densité')+
+  geom_density(alpha=.2, fill="steelblue") + theme(axis.text.y = element_blank())
 plotMas 
+plotCorrelation <- ggplot(data, aes(x=Longueur.total,Pourcentage.bases.masquées)) + geom_point()
+plotCorrelation
 
 
+dataDistribution <- dataD[order(dataD$Id.souche),]
+rownames(dataDistribution) <- dataDistribution$Id.souche
+dataQuality1 <- dataQuality[order(rownames(dataQuality)),]
+dataqualite <- dataQ
+dataGene <- read.table('Documents/count_gene.txt', header = F, sep = '\t')
+rownames(dataGene) <- dataGene$V1
+dataGene <-dataGene[order(rownames(dataGene)),]
 
+library("FactoMineR")
+library("factoextra")
+dataFrame<-data.frame(matrix(NA,ncol=18,nrow=nrow(dataDistribution)))
+for (i in 1:nrow(dataDistribution))
+{
+  print(strsplit(rownames(dataDistribution)[i],'_')[[1]][1])
+  #print(dataGene[strsplit(rownames(dataDistribution)[i],'_')[[1]][1],])
+  dataFrame[i,] <- c(dataDistribution[i,],dataQuality1[rownames(dataDistribution)[i],],dataGene[strsplit(rownames(dataDistribution)[i],'_')[[1]][1],])
+}
+rownames(dataFrame) <- rownames(dataDistribution)
+colnames(dataFrame) <- c(colnames(dataDistribution),colnames(dataQuality1),'id','NbGene')
+dataFrame <- dataFrame[,c(2:15,18)]
+res.pca <- PCA(dataFrame, graph = FALSE)
 
+fviz_pca_var(res.pca, col.var = "black")
+fviz_pca_var(res.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE # Évite le chevauchement de texte
+)
 
+dataFrame2 <- dataFrame[,c(2,5,7,8,11,15)]
+res.pca <- PCA(dataFrame2, graph = FALSE)
 
-
-
-
-
-
-
-
-
-
-
+fviz_pca_var(res.pca, col.var = "black")
+fviz_pca_var(res.pca, col.var = "cos2",
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE # Évite le chevauchement de texte
+)
